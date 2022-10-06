b06e5a880d30467e47272f3b635578bf7923404e
devpr <devpr@10.0.0.153>
2022-10-06 17:50:32 -0400

    commit for the assignment

---
+++ lsp_poli/src/org/howard/edu/lsp/assignment4/test/Driver.java
@@ -0,0 +1,124 @@
+package org.howard.edu.lsp.assignment4.test;
+
+import org.howard.edu.lsp.assignment5.implementation.IntegerSetException;
+import org.howard.edu.lsp.assignment5.implementation.IntegerSet;
+
+/**
+ * @author Prem Oli
+ * devpremoli
+ */
+
+public class Driver {
+	public static void main (String[] args) throws IntegerSetException {
+	IntegerSet set_a = new IntegerSet();
+	
+	//adds the value to the set_a
+	set_a.add(0);
+	set_a.add(1);
+	set_a.add(2);
+	
+	System.out.println("Value of set_a is:"+ " " + set_a.toString());
+	
+	//returns smallest value of set_a
+	System.out.println("Smallest value of set_a is:"+ " " + set_a.smallest());
+	
+	//returns largest value of set_a
+	System.out.println("Largest value of set_a is:"+ " " + set_a.largest());
+	
+	//checks contains
+	System.out.println("set_a.contains(6) returns: "+ " " + set_a.contains(6));
+	System.out.println("set_a.contains(1) returns: "+ " " + set_a.contains(1));
+
+	IntegerSet set_b = new IntegerSet();
+	set_b.add(4);
+	set_b.add(5);
+
+	//checks union
+	System.out.println("union of set_a and set_b");
+	System.out.println("Value of set_a is:" + set_a.toString());
+	System.out.println("Value of set_b is:" + set_b.toString());
+	set_a.union(set_b);// union of set_a and set_b
+	
+	// result of union of set_a and set_b
+	System.out.println("Result of union of set_a and set_b" + set_a.toString());
+	
+	set_b.add(1);
+	set_b.add(2);
+	System.out.println("Value of set_a is:" + set_a.toString());
+	System.out.println("Value of set_b is:" + set_b.toString());
+	
+	//intersection
+	set_a.intersect(set_b);
+	
+	//result of intersection of set_a and set_b
+	System.out.println("Result of intersection of set_a and set_b" + set_a.toString());
+
+	//clear
+	set_a.clear();
+	System.out.println("Value of set_a after clearing is:" + set_a.toString());
+	
+	//remove
+	System.out.println("Value of set_b is:" + set_b.toString());
+
+	set_b.remove(1);
+	System.out.println("Value of set_b after removing is:" + set_b.toString());
+
+	//difference
+	set_a.add(5);
+	set_a.add(6);
+	System.out.println("Value of set_a is:" + set_a.toString());
+	System.out.println("Value of set_b is:" + set_b.toString());
+	set_a.diff(set_b);
+	System.out.println("Result of difference of set_a and set_b is: " + set_a.toString());
+    
+	
+	set_a.clear();
+	set_b.clear();
+	// checks if set_a is empty
+	System.out.println("set_a.isEmpty() after clearing: "+ set_a.isEmpty());
+	
+	//checks IntegerSetException while removing from empty set
+	try {
+		set_a.remove(0);
+	}
+	catch (IntegerSetException e) {
+		System.out.println(e.getMessage());
+
+	}
+	
+	//checks IntegerSetException while finding greatest from empty set
+	try {
+		set_a.largest();
+	}
+	
+	catch (IntegerSetException e) {
+		System.out.println(e.getMessage());
+
+	}
+	
+	//checks IntegerSetException while finding smallest from empty set
+	 try {set_a.smallest();}
+	 catch (IntegerSetException e) {
+			System.out.println(e.getMessage());
+
+		}
+	 set_a.add(10);
+	 set_a.add(4);
+	 System.out.println("Value of set_a after adding 10 and 4 is:" + set_a.toString());
+
+	 System.out.println("The length of set_a is :" + set_a.length());
+	 
+	 // removes the value from a set that which is not there
+	 System.out.println("Value of set_a is:" + set_a.toString());
+	 set_a.remove(0);
+	 System.out.println("Value of set_a after removing 0 which is not there is:" + set_a.toString());
+	 
+	 //equals
+	 set_b.clear();
+	 set_b.add(10);
+	 set_b.add(4);
+	 System.out.println("Value of set_a is:" + set_a.toString());
+	 System.out.println("Value of set_b is:" + set_b.toString());
+	 set_a.equals(set_b);
+	 System.out.println("Value of set_b after running set_b.equals(set_b) is:" + set_a.equals(set_b));
+}}
---
+++ lsp_poli/src/org/howard/edu/lsp/assignment5/implementation/IntegerSet.java
@@ -0,0 +1,128 @@
+package org.howard.edu.lsp.assignment5.implementation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+/**
+ * @author Prem Oli
+ * devpremoli
+ */
+
+public class IntegerSet {
+	private ArrayList<Integer> MySet = new ArrayList<>();
+	
+	public IntegerSet(){
+		
+	};
+	
+	
+	
+	
+	/**
+	 * getter to retrieve our ArrayList
+	 * @return the ArrayList set so that we can use it to reference other methods 
+	 */
+	public ArrayList<Integer> getSet() {
+		return this.MySet;
+	}
+	
+	//empties the IntegerSet to clear all the contents using the .clear() method of ArrayList
+	public void clear() {
+		MySet.clear();
+	}
+	
+	//gives the length of IntegerSet
+	//returns length of the IntegerSet, using .size() method of ArrayList 
+	public int length() {
+		return this.getSet().size();
+	}
+	
+    // returns true if the 2 sets are equal, false otherwise;
+	public boolean equals(IntegerSet OtherSet) {
+		if( OtherSet.length()!=this.length()){
+			return false;
+	
+		}
+		else {
+			for (int i = 0; i < OtherSet.length(); i++) {
+				if (!MySet.contains(OtherSet.getSet().get(i))) {	
+					return false;
+			}
+		};
+		return true;
+		
+		
+	}
+}; 
+
+ 	//checks if the value is in the set
+	//returns true if the IntegerSet contains the number, else false by using ArrayList's .contain() function 
+	public boolean contains(int number) {
+		return this.getSet().contains(number);
+	}
+	
+	// Returns the largest item in the set; Throws a IntegerSetException if the set is empty 
+	public int largest() throws IntegerSetException {
+
+		if (this.getSet().isEmpty()) {
+			throw new IntegerSetException("An empty set can't have largest element!");
+		}
+		return Collections.max(this.getSet());
+		
+	} 
+
+	public int smallest() throws IntegerSetException {
+		
+
+		if (this.getSet().isEmpty()) {
+			throw new IntegerSetException("An empty set can't have smallest element!");
+		}
+		
+		return Collections.min(this.getSet());
+
+		}
+	
+	public void add(int item) {
+		if (!this.getSet().contains(item)) {
+			this.getSet().add(item);
+		}
+		
+	}
+	
+	public void remove(int item) throws IntegerSetException {
+		if (this.getSet().isEmpty()) {
+			throw new IntegerSetException("Can't remove from an empty set");
+		}
+		if (this.getSet().contains(item)) {
+			this.getSet().remove(this.getSet().indexOf(item));
+		}
+	}
+	
+	public void union(IntegerSet intSetb) {
+		if (!this.getSet().equals(intSetb.getSet())) {
+			
+			for (int i = 0; i < intSetb.getSet().size(); i ++) {
+				this.add(intSetb.getSet().get(i));
+			}	
+		}
+	}
+	
+	public void intersect(IntegerSet intSetb) {
+		this.getSet().retainAll(intSetb.getSet());
+		
+	}
+	
+	public void diff(IntegerSet intSetb) {
+		this.getSet().removeAll(intSetb.getSet());
+	};
+	
+	public boolean isEmpty() {
+		return this.getSet().isEmpty();
+	}
+	
+	public String toString() {
+		return this.getSet().toString();
+	}
+
+}
